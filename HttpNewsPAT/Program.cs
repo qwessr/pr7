ba5f26685578465df1e3606d8c11d33a9e74d042
Iliya12 <tankist228121234567890@gmail.com>
2025-12-09 18:47:13 +0500

    Получение печеньки авторизации. Капустин

--- HttpNewsPAT/Program.cs
+++ HttpNewsPAT/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,6 +13,7 @@ namespace HttpNewsPAT
     {
         static void Main(string[] args)
         {
+            SingIn("emilys", "emilyspass");
             WebRequest request = WebRequest.Create("https://permaviat.ru/");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Console.WriteLine(response.StatusDescription);
@@ -25,5 +27,42 @@ namespace HttpNewsPAT
             Console.Read();
 
         }
+        public static void SingIn(string username, string password)
+        {
+            CookieContainer cookieContainer = new CookieContainer();
+            string url = "";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = cookieContainer;
+
+            string postData = $"username={username}&password={password}";
+            byte[] data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = data.Length;
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            using (HttpWebResponse responseCookieGeter = (HttpWebResponse)request.GetResponse())
+            {
+                Debug.WriteLine($"Статус выполнения: {responseCookieGeter.StatusCode}");
+                string cookieHeader = responseCookieGeter.Headers["Set-Cookie"]; ;
+
+                Uri uri = new Uri(url);
+                CookieCollection cookies = cookieContainer.GetCookies(uri);
+
+                Console.WriteLine("COOKIES: ");
+                foreach (Cookie cookie in cookies)
+                {
+                    Console.WriteLine($"{cookie.Name}: \n{cookie.Value}");
+                }
+            }
+
+        }
     }
 }
+  
